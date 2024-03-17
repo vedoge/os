cab85ebc736ee39c511e6eadcf5d5440b64eed72
Vedant Ganesh <verdant.ganesh26@gmail.com>
2024-03-18 00:58:36 +0800

    a 16-bit kernel that works

--- source/kernel.asm
+++ source/kernel.asm
@@ -1,18 +1,26 @@
 bits 16
-org 0x1000
+org 0
 entry:
-	PUSH CS
-	POP DS
-	MOV SI, STRING
-	CALL BPRINT
-	CLI
-	HLT
-STRING: DB "HELLO WORLD",13, 10, 0
-BPRINT: 
-	MOV AH, 0x0E
-	LODSB
-	CMP AL, 0
-	JE END
-	INT 0x10
-END:
-	RET
+	sti		;//restore interrupts
+	push cs		
+	pop ds		;//CS=DS for addressing
+	mov si, string	;//address of string in SI
+	call bprint	;//call our routine
+	cli		;//no interrupts
+	hlt		;//halt
+bprint:
+	;//function head.
+	cld		;//DF=0 so lodsb moves forward through memory.
+.bprintloop:
+	lodsb		;//mov byte [ds:si], al; inc si
+	cmp al, 0	;//assuming null-terminated string
+	je .bprintdone	;//if it is, end
+	mov ah, 0x0e	//Write to screen
+	int 0x10	;//write to screen
+	jmp .bprintloop	;//loop for next character
+.bprintdone:
+	std		;//DF=1 for standards (stack grows downwards)
+	ret		;//quick end
+;//data area
+string: db "hello world!", 13, 10, 0
+	
