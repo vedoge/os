68301798e6823718bdec479ab6798dfebd4a4ce9
Vedant Ganesh <verdant.ganesh26@gmail.com>
2024-03-17 21:05:39 +0800

    Stub kernel

--- source/kernel.asm
+++ source/kernel.asm
@@ -1,30 +1,18 @@
-;//Note to self: 
-;//This program loads into memory as normal, but for whatever reason fails to print. 
-;//Whatever, i'll fix it tomorrow. 
-;//VG- 23:04 UTC+08
-ORG 0x20000
-;//The below order is critical for proper functioning of the operating system.
-;//this system adopts a model similar to that of Unix.
-JMP ENTRY
-ENTRY:
-	MOV AX, 0x2000
-	CLI
-	MOV SS, AX
-	MOV SP, 0
-	MOV BP, SP
-	STI
+bits 16
+org 0x1000
+entry:
 	PUSH CS
 	POP DS
-	MOV SI, MSG
+	MOV SI, STRING
 	CALL BPRINT
 	CLI
 	HLT
-BPRINT:
+STRING: DB "HELLO WORLD",13, 10, 0
+BPRINT: 
 	MOV AH, 0x0E
 	LODSB
 	CMP AL, 0
-	JE .DONE
+	JE END
 	INT 0x10
-	JMP BPRINT
-.DONE:	RET
-MSG:	DB "YAYYYYYY! FIRST STAGE OF OSDEV COMPLETE!", 0
+END:
+	RET
