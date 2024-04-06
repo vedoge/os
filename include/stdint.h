257979ca418d70f6822142407e009901d6e02b90
Vedant Ganesh <vganesh@debian-BULLSEYE-live-builder-AMD64>
2024-04-06 23:27:19 +0800

    added size_t typedef

--- include/stdint.h
+++ include/stdint.h
@@ -6,5 +6,6 @@ typedef signed long	int64_t;
 typedef unsigned char 	uint8_t;
 typedef unsigned short	uint16_t;
 typedef unsigned int	uint32_t;
-typedef unsigned long	int64_t;
+typedef unsigned long	uint64_t;
+typedef unsigned char	size_t;
 
