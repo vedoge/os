8e8d117be5a4a2760a8423f3fa7793e26c404c3c
Vedant Ganesh <vganesh@debian-BULLSEYE-live-builder-AMD64>
2024-04-06 23:28:20 +0800

    vga_puts now returns actual number of characters printed

--- include/vga.h
+++ include/vga.h
@@ -1,4 +1,7 @@
 #define __VGA_H
+#ifndef __STDINT_H
+#include "stdint.h"
+#endif
 #define VGA_BLACK	0x0
 #define VGA_BLUE	0x1
 #define VGA_GREEN 	0x2
@@ -8,12 +11,12 @@
 #define VGA_BROWN 	0x6
 #define VGA_WHITE	0x7
 #define VGA_GRAY	0x8
-#define VGA_BRIGHT	0x8				//VGA_BRIGHT is a bitmask to be combined with the other colours to give bright versions
-#define VGA_BLINK	0x80
-#define VGA_COLUMNS	80
-#define VGA_ROWS	25
-#define VGA_ATTRIB(fg,bg) fg | (bg<<8)
-extern "C" void vga_putchar(uint8_t uc, uint8_t attrib, uint8_t x, uint8_t y); 
-extern "C" void vga_puts(unsigned char * string, uint8_t attrib, uint8_t x, uint8_t y);
-extern "C" size_t strlen(const char * string);
+#define VGA_BRIGHT	0x8				/* VGA_BRIGHT is a bitmask to be combined with the other colours to give bright versions */
+#define VGA_BLINK	0x80				/* when the bitmask is applied to a background, 0x8 is interpreted as "blink". Otherwise, it is "bright" */
+static const uint8_t VGA_COLS = 80;
+static const uint8_t VGA_ROWS = 25;
+#define VGA_ATTRIB(bg,fg) fg | (bg<<4)			/* pack both into one byte */
+extern void vga_putchar(uint8_t uc, uint8_t attrib, uint8_t x, uint8_t y); 
+extern size_t vga_puts(const char * string, uint8_t attrib, uint8_t x, uint8_t y);	/* returns the number of characters printed */
+extern size_t strlen(const char * string);
 
