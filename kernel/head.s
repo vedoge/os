62116a0b5aed539ab229f18b8a3aec9580f1e995
Vedant Ganesh <verdant.ganesh26@gmail.com>
2024-04-24 17:58:39 +0800

    begun to write code for PIC initialisation

--- kernel/head.s
+++ kernel/head.s
@@ -10,6 +10,21 @@ _entry:
 	//everything else is demand-paging
 	//device drivers have a reserved section somewhere for a jump table
 	//rest is paged
+	//reprogram 8259s
+	inb $0x20 ,%al		//save the master
+	push %eax		//on the stack
+	inb $0xa0, %al		//save the slave
+	push %eax		//on the stack
+	movb  $1, %al		//ICW1 = ICW4_PRESENT
+	outb $0x20, %al		//send out
+	nop, nop
+	outb $0xa0, %al 
+	movb $0x20, %al		//ICW2: vector offset
+	outb $0x21, %al		//PIC1_DATA
+	or $0xf, %eax		//add for the vector offset of sub PIC
+	outb $0xa0, %al		//send to sub PIC
+	//set up IDT
+	movl
 	movl $0x0fff, %edi
 	movl $0x0fff, %ecx
 	movl $0x2, %eax		//no page directory
@@ -28,5 +43,4 @@ rep	stosd
 	or $0x80000000, %eax
 	movl %eax, %cr0 
 	movl $0, %eax
-	movl %eax, %cr3		//paging table 
-	movl %
+	movl %eax, %cr3		//cr3 points to page table
