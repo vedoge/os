beafd96ba887659a6ceee4067bcb1c8754f6b12f
Vedant Ganesh <>
2024-04-25 22:57:51 +0800

    Minor file renaming / reorg

---
+++ kernel/main.c
@@ -0,0 +1,6 @@
+#include <vga.h>
+int main() { 
+	char * hello = "hi!";		//test string
+	vga_puts(hello,VGA_ATTRIB(VGA_BLINK | VGA_BLACK, VGA_BRIGHT | VGA_WHITE), 0, 0); 
+	asm("cli;hlt");			//stop here
+}
