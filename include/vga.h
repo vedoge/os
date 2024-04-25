4f8d0cea05070478afb897d463466bfa4f48273f
Vedant Ganesh <>
2024-04-25 22:56:26 +0800

    Changed VGA_BLINK so it fits into VGA_ATTRIB

--- include/vga.h
+++ include/vga.h
@@ -12,7 +12,7 @@
 #define VGA_WHITE	0x7
 #define VGA_GRAY	0x8
 #define VGA_BRIGHT	0x8				/* VGA_BRIGHT is a bitmask to be combined with the other colours to give bright versions */
-#define VGA_BLINK	0x80				/* when the bitmask is applied to a background, 0x8 is interpreted as "blink". Otherwise, it is "bright" */
+#define VGA_BLINK	0x8				/* when the bitmask is applied to a background, 0x8 is interpreted as "blink". Otherwise, it is "bright" */
 const uint8_t VGA_COLS = 80;
 const uint8_t VGA_ROWS = 25;
 #define VGA_ATTRIB(bg,fg) fg | (bg<<4)			/* pack both into one byte */
