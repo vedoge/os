f48c2a14ab697ba512ed3c18539fc86543905d85
vganesh <vganesh@hmm>
2024-06-03 11:50:34 +0800

    reorg

---
+++ kernel/drivers/vga/vga.c
@@ -0,0 +1,5 @@
+#include "vga/vga.h"
+static const void* vga_buffer = 0xb8000;
+void vga_putchar(uint8_t uc, uint8_t attrib, uint8_t x, uint8_t y) {
+	
+}
---
+++ kernel/include/config.h
@@ -0,0 +1,6 @@
+#define __CONFIG_H
+#define MAXMEM 16777216		// bytes of memory mapped 
+/*
+A static memory configuration is used as the amount of memory an embedded system has is unlikely to change over time.
+For hobbyists, it is trivial to change this number in the source code, so this simple solution is good enough. 
+*/
