9810c892fe4b94c5b97f64432080eb17d593ca0e
vganesh <vganesh@hmm>
2024-06-03 13:01:17 +0800

    Major reorg - move headers to "include"

---
+++ kernel/drivers/i82077a/i82077a.c
@@ -0,0 +1,2 @@
+#include "stdint.h"
+#include "i82077a.h"
--- kernel/drivers/i8259/i8259.c
+++ kernel/drivers/i8259/i8259.c
@@ -1,14 +1,15 @@
+/* the driver for the intel i8259 PIC */
+#include "stdint.h"
+#include "i8259.h"
 void eoi(uint8_t irq) {
 	if(irq & 0x8) outb(PIC2_COM, 0x20);
 	outb(PIC1_COM, 0x20);
 }
-void pic_init(void) {
-	//TODO
-}
+// PIC init done initially by PICS
 void set_mask(uint8_t irq) {
 	//setting a mask means it is set high
 	uint8_t val;
-	if (irq ^ 8) {
+	if (irq & 8) {						/* high IRQ - PIC2*/
 		inb (PIC2_DATA, mask);
 	outb(PIC2_DATA,mask | (1 << (irq-8)));
 	}
