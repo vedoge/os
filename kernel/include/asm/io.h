a1bc4a1a47fe431fabe78e2a21e38c225a2c0a44
vganesh <vganesh@hmm>
2024-06-03 11:50:46 +0800

    corrected extraneous memory clobber

--- kernel/include/asm/io.h
+++ kernel/include/asm/io.h
@@ -3,10 +3,10 @@
 	asm volatile (	"inb %%dx, %%al;"  	\
 			:"=a" (val)		/* map al to var that stores it is the value being received */ \
 			:"d" (port) 		/* map the port to dx, as it is the port being listened to */ \
-			: "memory"		/* memory clobber */)
+			: 			/* no clobber */)
 #define outb(port, val) /* send value val to port port */ \
 	asm volatile (	"outb %%dx, %%al" \
 			:"=a" (val) 		/* val mapped to al (data being sent) */ \
 			: "a" (port) 		/* port mapped to dx (port in question) */ \
-			: "memory" 		/* memory clobber*/)
+			:  		/* no clobber*/)
 
