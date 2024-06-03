ca091946c9cf13e5b100c26a51aa21da27408829
vganesh <vganesh@hmm>
2024-06-03 11:38:40 +0800

    Finished paging and kernel entry jump.

--- kernel/boot/stage2.asm
+++ kernel/boot/stage2.asm
@@ -1,67 +1,62 @@
+; caps = real mode code
+; lowercase = pmode / paged code
 bits 16
-org 0x4000
+org 0xC000
 jmp entry16
 
-pic1_com	equ 0x20
-pic1_data	equ 0x21
-pic2_com	equ 0xa0
-pic2_data	equ 0xa1
+
 
 
 ; data area
-FNAME: DB "KERNEL  BIN"
-CLUST: DW 0
+	FNAME: DB "KERNEL  BIN"
+	CLUST: DW 0
 ; general definitions
-stage2_base	equ 0x4000
-ROOTDIRENTRIES	EQU 224
+	stage2_base	equ 0xc000	; where we are
+	pt_base		equ 0x1000	; where our page tables begin
+	pt_ceil		equ 0x6ffc	; where our page tables end
+	ROOTDIRENTRIES	EQU 224		; no. of FAT rootdir entries
+	kern_base	equ 0x40000	; phys / idpaged kernel addr
+	vga_off 	equ 0xb8000	; vga mmio text buffer addr
 ; elf definitions
 
 ; ELF header
 
-e_ident		equ 0		; 0	begins 0x7f 'E' 'L' 'F'
-e_type		equ 0x10	; 16	must be 2 (no relocations)
-e_machine	equ 0x12	; 18	must be 0x03 for us
-e_version	equ 0x14	; 20	must be 1
-e_entry		equ 0x18	; 24
-e_phoff		equ 0x1c	; 28
-e_shoff		equ 0x20	; 32
-e_flags		equ 0x24	; 36
-e_ehsize	equ 0x28	; 40
-e_phentsize	equ 0x2a	; 42
-e_phnum		equ 0x2c	; 44
-e_shentsize	equ 0x2e	; 46
-e_shnum		equ 0x30	; 48
-e_shstrndx	equ 0x32	; 50
-e_ehdrend	equ 0x34	; 52
+	e_ident		equ 0		; 0	begins 0x7f 'E' 'L' 'F'
+	e_type		equ 0x10	; 16	must be 2 (no relocations)
+	e_machine	equ 0x12	; 18	must be 0x03 for us
+	e_version	equ 0x14	; 20	must be 1
+	e_entry		equ 0x18	; 24
+	e_phoff		equ 0x1c	; 28
+	e_shoff		equ 0x20	; 32
+	e_flags		equ 0x24	; 36
+	e_ehsize	equ 0x28	; 40
+	e_phentsize	equ 0x2a	; 42
+	e_phnum		equ 0x2c	; 44
+	e_shentsize	equ 0x2e	; 46
+	e_shnum		equ 0x30	; 48
+	e_shstrndx	equ 0x32	; 50
+	e_ehdrend	equ 0x34	; 52
 
 ; program header
 
-p_type		equ 0x0
-p_offset	equ 0x2
-p_vaddr		equ 0x6
-p_paddr		equ 0xa
-p_filesz	equ 0xe
-p_memsz		equ 0x10
-p_flags		equ 0x12
-p_align		equ 0x14
+	p_type		equ 0x0
+	p_offset	equ 0x4
+	p_vaddr		equ 0x8
+	p_paddr		equ 0xc
+	p_filesz	equ 0x10
+	p_memsz		equ 0x14
+	p_flags		equ 0x18
+	p_align		equ 0x1c
+	p_size		equ 0x20
 
-; section header
+; PIC offsets 
+	pic1_com	equ 0x20
+	pic1_data	equ 0x21
+	pic2_com	equ 0xa0
+	pic2_data	equ 0xa1
 
-sh_name		equ 0x0		; 0
-sh_type		equ 0x2		; 2
-sh_flags	equ 0x4		; 4
-sh_addr		equ 0x6		; 6
-sh_off		equ 0xa
-sh_size		equ 0xe
-sh_link		equ 0x10
-sh_info		equ 0x12
-sh_addralign	equ 0x14
-sh_entsize	equ 0x16
-
-
-entry16:
-; copied from the bootloader
-; load the kernel image proper at 0x8000 (temporary location)
+entry16:				; load the kernel ELF at 0x40000
+	; copied from the bootloader
 READROOTDIR:
 	PUSH 0x07E0
 	POP ES
@@ -84,18 +79,18 @@ REP	CMPSB			; compare the CX characters at DS:SI and ES:DI
 	MOV DI, AX		; next entry
 	XCHG CX, DX		; external index (looping through the root directory entries themselves)
 LOOP	SEARCH
-LOADFAT:
+LOADFAT:			; find the kernel cluster
 	MOV AX, [ES:DI+0xF]	; 11 (file length) + 15 (random info) = 26 (cluster offset)
 	PUSH AX
-SETUP:
+SETUP:				; setup to read kernel
 	; define the segmentation of the kernel. 
 	PUSH 0x09A0		; FAT location
 	POP DS			; in DS
-	PUSH 0xB00		; kernel loaded at b000(610 kb limit)
+	PUSH 0x4000	; kernel loaded at 40000 (610 KiB limit)
 	POP ES			; in ES
 	XOR BX, BX		; buffer address pointer is ES:0
 
-LOAD:
+LOAD:				; load the kernel file into memory
 	POP AX
 	PUSH AX
 
@@ -131,7 +126,7 @@ EVEN:
 	JGE LOADED
 	PUSH AX			; save cluster for next iteration
 	JMP LOAD 
-LBACHS:	
+LBACHS:				; convert LBA to CHS addressing for INT 13h
 	; function is specific to 3.5" diskette format and must be modified to fit larger and more general disks. 
 	; puts CHS parameters in the right place for INT 0x13 to use
 	PUSH BX		; put away BX as a garbage register
@@ -152,7 +147,7 @@ LBACHS:
 	RET		; bye
 
 
-LOADED:				; kernel is loaded - we just need to arrange it in memory using the ELF headers
+LOADED:				; arrange kernel in memory
 	; jump into protected mode
 	; put the gdt into place
 	; page
@@ -163,15 +158,11 @@ LOADED:				; kernel is loaded - we just need to arrange it in memory using the E
 	pop ds
 	push ds			; whoops - forgot these! Doesn't work otherwise :)
 	pop es
-;------------------------------------------------------------------------------
 	lidt [idtr]
-;------------------------------------------------------------------------------
 	lgdt [gdtr] 
-;------------------------------------------------------------------------------
 	mov eax, cr0
 	or eax, 1
 	mov cr0, eax
-;------------------------------------------------------------------------------
 	jmp 0x8:start		; reload CS and begin execution in 32-bit mode
 bits 32
 start:
@@ -180,6 +171,8 @@ start:
 	mov es, eax
 	mov fs, eax
 	mov ss, eax
+	mov esp, 0xf000
+	mov ebp, esp
 	mov esi, message
 	call vgaprint
 	; PIC initialisation
@@ -223,133 +216,255 @@ start:
 	jmp $+2
 	jmp $+2
 	out pic2_data, al	;same for pic2
-align 	4			; something something funky pointer arithmetic
-pg_overwrite_limit:		; maximum offset that the page table can overwrite
+
 ; paging code
 	std			; ensure that rep stosd goes downwards in memory (faster and easier to process)
-	mov edi, 0x3ffc
+	mov edi, 0x0ffc
 	mov eax, 0x00000002
 	mov ecx, edi
 	shr ecx, 2		; ecx = ecx div 4
 	inc ecx			; include 0th entry
 rep	stosd			; rep stosd
 	; the range 0x0000-0x0fff now contains 1000 not-present page directory entries
-	mov edi, pg_overwrite_limit
+	mov edi, pt_ceil
 	mov ecx, edi
-	sub ecx, stage2_base
-	shr ecx, 2
+	sub ecx, pt_base
+	shl ecx, 10		; ecx = page frame / bits 31-12 of physaddr
+	shr ecx, 12		; net shift right of 2 (division by 4 of the pointer)
 
-	; store the memory address that will be mapped to 0xb8000 (VGA page)
-	shl ecx, 12		; shift left to give memory address from index
-	mov dword [vga_off], ecx
-	shr ecx, 12 		; undo the shift
-
+	mov dword [highest_phys_frame], ecx
+				; store the highest mapped physical frame 
 	inc ecx			; for entry 0
+
+;	hlt			; NOTE DEBUGGING STOP
 .loop:
 	mov eax, ecx
-	dec eax			; so 0x0 is mapped to 0x0 and not 0x1000
+	dec eax			; so 0x0 is mapped to 0x0 and not pt_base
 	shl eax, 12		; 3 nibbles left (eax = ecx * 4KiB)
 	or eax, 0x03		; mark present and writable
 	stosd
-	loop .loop
-	cli
-	
-;	hlt			; NOTE DEBUGGING STOP
-	mov dword [pg_overwrite_limit], 0xb8003
+loop	.loop
 
-	; we need to fill it with the corresponding memory addresses
-	; this is a fine way of doing it imho - the PIC code alone is 70 bytes. 
-	; the PIC stuff alone gives us enough space to identity page 
-	; from 0x0 to 0x11000 (which is enough for now). 
-	; my educated guess is that there's 300 bytes of code above us,
-	; which should map everything from 0x0 to 0x4b000 (which EVERY OS has)
-	; additionally, a page must be reserved for the VGA system at 0xb8000
-	; further map reservations should probably be made by most OSes from 
-	; BIOS/CMOS/Multiboot memory maps 
-	; but that's mostly for things that we won't interfere with
-	; as a simple embedded system, so i'll ignore it for now. 
-	; We'll later remap some of low memory up to 0xC0000000 (higher half)
-	; for simplicity, but for now it's not worth the hassle
-	; we identity page the first few KiB of memory to remove the need
-	; to translate virtual addresses to physical ones in assembly, 
-	; but C should deal with this just fine. 
-	; note- addresses 0x00000000-0x00044000 are available and id paged
-	; (~1/3rd the real address space). 
-	; address 0x00045000 is mapped to the VGA page at phys. 0xb8000. 
-	; the kernel will upon initialisation fill this section with pages
-	; until 0x5000, which gives us memory access until 0x0040'0000 
-	; (4 MiB), and map itself to 0xC0000000 (higher half) 
-	mov eax, 0x4000		; address of first page table
-	or eax, 3		; present, rw
-	mov dword [ds:0], eax		; place in page directory
+;	cli
+;	hlt			; NOTE DEBUGGING STOP
+; page directory code
+	mov ecx, stage2_base
+	sub ecx, pt_base
+	shr ecx, 0xc
+	lea edi, [ecx*4-4]
+.repeat:
+	mov eax, ecx
+	shl eax, 12
+	or eax, 3
+	stosd
+	loop .repeat
+
+;	hlt			; NOTE DEBUGGING STOP
+;	switch to paged
+	; load base of page directory into cr3
 	mov ecx, cr3
 	xor ecx, ecx
 	mov cr3, ecx		; flush tlb
+	; enable PG bit in cr0
 	mov eax, cr0
 	or eax, 0x80000000
 	mov cr0, eax		; PG_ENABLE
 	jmp 0x8:pg_enabled
 pg_enabled:
+	; if we page fault now, we're toast
+
 	cli
-	; map VGA to address
-	mov esi, message
+	; show something on screen (debugging purposes)
+	mov esi, paged
 	call vgaprint
+;	hlt			; debugging purposes
+
+; check elf magic number
+	mov esi, 0x40000
+	push esi
+	mov edi, elf_mag
+	mov ecx, 4
+	cld
+	rep cmpsb
+	jne noelf
+	std
+	pop esi
+; prepare to loop through the program header table
+	movzx ecx, word [esi+e_phnum]
+	mov eax, [esi+e_phoff]	; store program header offset
+	lea esi, [esi+eax*1-p_size]
+				; get the effective address of the ph table-32
+phdr:
+	add esi, p_size		; search next table
+	cmp dword [esi], 1
+	je .loadseg
+	loopnz phdr		; null/unknown entry, ignore
+	; error- a segment that shouldn't be here
+.loadseg:
+	push dword [esi+p_align]
+	push dword [esi+p_memsz]
+	push dword [esi+p_vaddr]
+	call modify_page_table	; modify the page table to fit the kernel layout
+	
+	mov ebx, [esi+p_align]	; alignment (most probably, page alignment
+				; as specified in the linker script)
+	mov eax, [esi+p_filesz] 
+	xor edx, edx
+	mov ecx, 4
+	div ecx
+	mov ecx, eax
+	mov edi, [esi+p_vaddr]
+	mov esi, [esi+p_offset]
+	add esi, 0x40000
+	cld
+	rep movsd
+	mov ecx, edx
+	rep movsb
+	std
+	cli
 	hlt
+	mov esi, 0x8048000
+	push 0x8
+	push dword [esi+e_entry]
+	retf
+	
+noelf:
+
+	mov esi, elferror
+	call vgaprint
+
+	cli
+	hlt		; end
+modify_page_table:
+	push ebp
+	mov ebp, esp
+	; first, compute the number of pages
+	mov eax, dword [ebp+12]
+	shr eax, 12		; divide by 0x1000 (4KiB, one page)
+	inc eax
+	mov dword [.nr_pages], eax
+	; get virtual address and compute the page directory entry first
+	mov eax, dword [ebp+8]	; p_vaddr
+	mov ebx, [ebp+16]
+	neg ebx
+	and eax, ebx		; probably unnecessary but
+	push eax		; push it
+	; now compute the page directory entry
+	and eax, 0xffc00000	; top 10 bits, mark present & writable
+	shr eax, 0x16		; turn into index for page directory
+	mov dword [eax*4], ((pt_ceil+4)|3)
+				; store configured ceiling + 4 and present / writable
+	pop eax
+	and eax,0x003ff000	; bits 21-12
+	shr eax, 12		; get page frame
+	mov ebx, dword [highest_phys_frame]
+	shl ebx, 12
+	push ecx
+	mov ecx, [.nr_pages]
+	lea edi, [eax*4+pt_ceil+4]
+.loop:			; allocate nr_pages
+	add ebx, 0x1000
+	mov eax, ebx		
+	or eax, 3		; present and writable
+	stosd
+	loop modify_page_table.loop
+	;endloop
+	shr ebx, 12
+	mov [highest_phys_frame], ebx
+				; store the highest physical frame mapped
+	pop ecx
+
+.done:
+	mov esp, ebp		; quickly clean up garbage on stack
+	pop ebp
+	std
+	ret
+.nr_pages: dd 0
+
+
+; PMode data/subroutines area
 message:
 	db "h", 0x8f,"e", 0x8f, "l", 0x8f, "l", 0x8f, "o", 0x8f, "!", 0x8f, 0, 0
 
+paged:	db "p", 0x8f, "a", 0x8f, "g", 0x8f, "e", 0x8f, "d", 0x8f, 0xa, 0x8f, 0, 0
+
+elferror:
+	db 'u', 0x8f, 'n', 0x8f, 'k', 0x8f, 'n', 0x8f, 'o', 0x8f, 'w', 0x8f
+	db 'n', 0x8f, ' ', 0x8f, 's', 0x8f, 'e', 0x8f, 'g', 0x8f, 'm', 0x8f
+	db 'e', 0x8f, 'n', 0x8f, 't', 0x8f, '!', 0x8f, 0, 0
+
+elf_mag:
+	db 0x7f, 'E', 'L', 'F' 
+
+
+
 vgaprint:
+	cli
 	push eax
-	push edx
+	push ebx
 	mov eax, [.idx]
-	mov edx, [vga_off]
-	lea edi, [edx+eax*2]
+	lea edi, [vga_off+eax*2]
 	xor ecx, ecx
 	not ecx
 	cld
 .loop:
+	
 	lodsw
 	cmp ax, 0
 	je .done
+	cmp al, 0xa
+	je .newline
 	stosw
 	inc word [.idx]
 	loop .loop
+
+.newline:
+	;hlt			; DEBUGGING STOP
+
+
+	mov eax, dword [.idx]
+	mov ebx, 80
+	xor edx, edx		; note: "div r/m32" uses edx:eax
+	div ebx			; as its implicit operand, so zero it.
+	cmp eax, 25		; end of screen
+
+;	cmovge eax, 0		; would simplify this structure, but cmovcc is
+				; P/K5+ only
+
+	jge .wrapscr		; print starting from next row col 0 again
+
+	add eax, 1		; next line (remainder is discarded)
+	mov ebx, 80
+	xor edx, edx
+
+	mul ebx			; need to zero edx here also for "mul r/m32"
+
+
+	lea edi, [vga_off+eax*2]; load our new pointer
+	mov dword [.idx], eax	; store as idx
+	loop vgaprint.loop	; affect ecx (count as character) 
+
+.wrapscr:
+	xor eax, eax
+	mov dword [.idx], eax
+	lea edi, [vga_off+eax*2]
+	loop vgaprint.loop		; clobber ecx (count as character)
+
 .done:
 	neg ecx
-	dec ecx
-	pop edx
-	pop eax
-	std
-	ret
-.idx:	dw 0
-%if 0				; "multiline comment" 
-pic1_com	equ 0x20
-pic1_data	equ 0x21
-pic2_com	equ 0xa0
-pic2_data	equ 0xa1
+	dec ecx			; account for the -1
 
-read_track	equ 0x2
-specify		equ 0x3
-sense_status	equ 0x4
-write_data	equ 0x5
-read_data	equ 0x6
-recalibrate	equ 0x7		; seek to 0
-sense_interrupt	equ 0x8
-write_del_data	equ 0x9
-read_id		equ 0xa
-read_del_data	equ 0xb
-format_track	equ 0xd
-dumpreg		equ 0xe
-seek		equ 0xf
-version		equ 0x10
-scan_equal	equ 0x11
-perpendicular	equ 0x12
-configure	equ 0x13
-lock		equ 0x14
-verify		equ 0x16
-scan_le		equ 0x19
-scan_he		equ 0x1d
-%endif
+	pop ebx			; also restore
+	pop eax
+
+	std
+
+	ret
+.idx: dd 0
+
+highest_phys_frame: dd 0
+
+
 gdt:
 	DQ 0
 
@@ -379,5 +494,5 @@ gdtr:
 
 idtr:	dw 0
 	dq 0
-vga_off: dd 0xb8000
+
 end:
