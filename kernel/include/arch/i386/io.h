2f489b478533295ae563ba8883c341b34754adb6
vganesh <vganesh@hmm>
2024-06-05 22:24:33 +0800

    CRLF changes and macros work now, hopefully

--- kernel/include/arch/i386/io.h
+++ kernel/include/arch/i386/io.h
@@ -1,12 +1,13 @@
 #define _IO_H
 #define inb(port,val) /* receive input from the port "port" in "val" */ \
-	asm volatile (	"inb %%dx, %%al;"  	\
+	asm volatile (	"inb %%dx, %%al;"  								\
 			:"=a" (val)		/* map al to var that stores it is the value being received */ \
 			:"d" (port) 		/* map the port to dx, as it is the port being listened to */ \
-			: 			/* no clobber */)
+			: 			/* no clobber */; \
+			)
 #define outb(port, val) /* send value val to port port */ \
 	asm volatile (	"outb %%al, %%dx" \
-			:
+			:			/* no output operands */		\
 			: "d" (port) 		/* port mapped to dx (port in question) */ \
 			  "a" (val) 		/* val mapped to al (data being sent) */ \
 			:  		/* no clobber*/)
