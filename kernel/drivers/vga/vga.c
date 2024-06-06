233fe3c19c436e752b077d8f3497516e6dd9f92c
vganesh <vganesh@hmm>
2024-06-06 20:43:48 +0800

    remove extraneous brackets around pointer

--- kernel/drivers/vga/vga.c
+++ kernel/drivers/vga/vga.c
@@ -1,24 +1,21 @@
-#include "vga/vga.h"
+#include "vga.h"
 #include "arch/i386/io.h"
-static const void* vga_buffer = 0xb8000;
+char * const vga_buffer = 0xb8000;
 static uint16_t index = 0;
 size_t strlen(const char * string) {
 	/* a standard algorithm. */
 	/* TODO implement in assembly to optimise speed */
 	size_t retval = 0;
-		__asm__ ("
-		 repnz scasb;
-		 not %%ecx;
-		 dec %%ecx;
-		"
-		: "=c" (retval)	/* output ecx in retval*/
-	
-		: "a" (0)
-		  "c" (-1)
-		  "S" (string)
-		: "ecx"		/* looping register */
-	
-		  "esi"		/* for repnz scasb */  
+		asm volatile ("	\
+		 repnz scasb;	\
+		 not %%ecx;	\
+		 dec %%ecx;	\
+		"		\
+		: "=c" (retval)	/* output ecx in retval*/ \
+		: "a" (0),				\
+		  "c" (-1),				\
+		  "S" (string)				\
+		 :	/* everything clobbered is listed above */  	\
 	);
 	return retval;
 	/*
@@ -28,8 +25,8 @@ size_t strlen(const char * string) {
 }
 size_t vga_puts(const char * string, uint8_t attrib, uint8_t x, uint8_t y) {
 	size_t i = 0;
-	for (; i <= strlen(uc); i++) {
-		if (*(string+i) == "\n") index = ((index / VGA_COLS)+1)*VGA_COLS;
+	for (; i <= strlen(string); i++) {
+		if (*(string+i) == '\n') index = ((index / VGA_COLS)+1)*VGA_COLS;
 		*(vga_buffer + index*2 + i*2) = ((attrib << 8) | *(string+i));
 	}
 	update_cursor(index);
@@ -42,9 +39,10 @@ void vga_putchar (uint8_t uc, uint8_t attrib, uint8_t x, uint8_t y) {
 	return;  
 
 }
-void update_cursor(size_t loc) {				/* move the cursor */
+void update_cursor(uint16_t loc) {				/* move the cursor */
 	outb(0x3D4, 0x0F);
-	outb(0x3D5, (uint8_t) (pos & 0xFF));
+	outb(0x3D5, (uint8_t) (loc & 0xFF));
 	outb(0x3D4, 0x0E);
-	outb(0x3D5, (uint8_t) ((pos >> 8) & 0xFF));
+	outb(0x3D5, (uint8_t) ((loc >> 8) & 0xFF));
 }
+
