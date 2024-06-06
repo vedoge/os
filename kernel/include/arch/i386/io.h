9a5a0791aa22e8c49256371639274a94ec367cf1
vganesh <vganesh@hmm>
2024-06-06 10:09:41 +0800

    (semi-) final version of io.h; no string instructions

--- kernel/include/arch/i386/io.h
+++ kernel/include/arch/i386/io.h
@@ -1,14 +1,15 @@
 #define _IO_H
 #define inb(port,val) /* receive input from the port "port" in "val" */ \
-	asm volatile (	"inb %%dx, %%al;"  								\
-			:"=a" (val)		/* map al to var that stores it is the value being received */ \
-			:"d" (port) 		/* map the port to dx, as it is the port being listened to */ \
+	asm volatile (	"inb %%dx, %%al"  								\
+			: "=a" (val)		/* map al to var that stores it is the value being received */ \
+			: "d" (port) 		/* map the port to dx, as it is the port being listened to */ \
 			: 			/* no clobber */; \
 			)
 #define outb(port, val) /* send value val to port port */ \
 	asm volatile (	"outb %%al, %%dx" \
 			:			/* no output operands */		\
-			: "d" (port) 		/* port mapped to dx (port in question) */ \
+			: "d" (port),		/* port mapped to dx (port in question) */ \
 			  "a" (val) 		/* val mapped to al (data being sent) */ \
-			:  		/* no clobber*/)
+			:  		/* no clobber*/ \
+			)
 
