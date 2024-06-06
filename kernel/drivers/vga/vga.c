639b0d4fd2c939b2c7196c62a38407f6bdc3fdf8
vganesh <vganesh@hmm>
2024-06-07 00:37:22 +0800

    FIxed bugs; works completely now

--- kernel/drivers/vga/vga.c
+++ kernel/drivers/vga/vga.c
@@ -1,20 +1,22 @@
 #include "vga.h"
 #include "arch/i386/io.h"
-char * const vga_buffer = 0xb8000;
-static uint16_t index = 0;
+char * const vga_buffer = 0xb8000;		/* vga mmio address */
+static uint16_t index = 0;			/* where our cursor is atm - must implement wraparound */
 size_t strlen(const char * string) {
 	/* a standard algorithm. */
 	/* TODO implement in assembly to optimise speed */
 	size_t retval = 0;
 		asm volatile ("	\
+		 cld;		\
 		 repnz scasb;	\
+		 std;		\
 		 not %%ecx;	\
 		 dec %%ecx;	\
 		"		\
 		: "=c" (retval)	/* output ecx in retval*/ \
 		: "a" (0),				\
 		  "c" (-1),				\
-		  "S" (string)				\
+		  "D" (string)				\
 		 :	/* everything clobbered is listed above */  	\
 	);
 	return retval;
@@ -26,8 +28,14 @@ size_t strlen(const char * string) {
 size_t vga_puts(const char * string, uint8_t attrib, uint8_t x, uint8_t y) {
 	size_t i = 0;
 	for (; i <= strlen(string); i++) {
-		if (*(string+i) == '\n') index = ((index / VGA_COLS)+1)*VGA_COLS;
-		*(vga_buffer + index*2 + i*2) = ((attrib << 8) | *(string+i));
+		if (*(string+i) == '\n') {
+			index = ((index / VGA_COLS)+1)*VGA_COLS;
+			update_cursor(index);
+			continue; 
+		}
+		index = index>=2000 ? 0 : index;		/* wrap around */ 
+		/* typecasting black magick incoming */
+		*(uint16_t *)(vga_buffer + index*2 + i*2) = (uint16_t)(*(string+i)) | ((uint16_t)(attrib) << 8);
 	}
 	update_cursor(index);
 	return i; 
