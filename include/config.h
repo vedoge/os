9ece3962cfc297d52eb0581b8e90117d814c805d
vganesh <vganesh@hmm>
2024-06-15 15:29:46 +0800

    config changes to support interrupts

--- include/config.h
+++ include/config.h
@@ -1,6 +1,22 @@
 #define __CONFIG_H
 #define MAXMEM 16777216		// bytes of memory mapped 
 /*
-A static memory configuration is used as the amount of memory an embedded system has is unlikely to change over time.
-For hobbyists, it is trivial to change this number in the source code, so this simple solution is good enough. 
+ * A static memory configuration is used
+ * as the amount of memory is unlikely to change over time. 
+ * For hobbyists, it is trivial to change this number 
+ * in the source code, so this simple solution is good enough. 
+*/
+#define NR_INTERRUPTS 256
+/* 
+ * map all the interrupt gates; most of them will probably be unused.
+ * Over time, I hope to develop an interrupt handler that can 
+ * kill the offending process and display a message to the console. 
+ * This would be a lot easier for the end user to cope with.
+*/
+#define NR_GDT_ENTRIES 7
+/*
+ * Using 7 GDT entries gives room for 5 user segments and 2 TSS
+ * which should be more than enough (if a call is nested) 
+ * more than 3 control transfers in, that call can be written
+ * in a much better way.
 */
