e1f2767dae07ce53e0809a53e2bb15cd29da14bc
vganesh <vganesh@hmm>
2024-06-18 22:48:08 +0800

    grown for some files

--- build.sh
+++ build.sh
@@ -1,11 +1,12 @@
 #!/bin/bash 
 nasm -f bin kernel/boot/stage2.asm -o stage2.bin 
-/usr/opt/cross/bin/i686-elf-gcc -static -fno-pie -I./include -nostdlib -mgeneral-regs-only -c kernel/main.c -o main.o
-/usr/opt/cross/bin/i686-elf-gcc -static -fno-pie -I./include -nostdlib -c kernel/drivers/vga/vga.c -o vga.o
-/usr/opt/cross/bin/i686-elf-gcc -static -fno-pie -I./include -nostdlib -c kernel/drivers/i8259/i8259.c -o i8259.o
-/usr/opt/cross/bin/i686-elf-gcc -static -fno-pie -I./include -nostdlib -mgeneral-regs-only -c kernel/arch/i386/interrupts.c -o interrupts.o
+/usr/opt/cross/bin/i686-elf-gcc -static -fno-pie -I./include -nostdlib -O0 -w -c kernel/main.c -o main.o
+/usr/opt/cross/bin/i686-elf-gcc -static -fno-pie -I./include -nostdlib -O0 -w -c kernel/hw/vga/vga.c -o vga.o
+/usr/opt/cross/bin/i686-elf-gcc -static -fno-pie -I./include -nostdlib -O0 -w -c kernel/hw/i8259/i8259.c -o i8259.o
+/usr/opt/cross/bin/i686-elf-gcc -static -fno-pie -I./include -nostdlib -O0 -w -mgeneral-regs-only -c kernel/arch/i386/interrupts.c -o interrupts.o
+/usr/opt/cross/bin/i686-elf-gcc -static -fno-pie -I./include -nostdlib -O0 -w -mgeneral-regs-only -c kernel/hw/i8042/i8042.c -o i8042.o
 # /usr/opt/cross/bin/i686-elf-gcc -static -fno-pie -I./include -nostdlib -c kernel/arch/i386/gdt.c -o gdt.o
-/usr/opt/cross/bin/i686-elf-ld main.o vga.o interrupts.o gdt.o i8259.o -e main -o main
+/usr/opt/cross/bin/i686-elf-ld main.o vga.o interrupts.o gdt.o i8259.o i8042.o -e main -o main
 
 sudo mount -o loop floppy.img floppy
 sudo cp stage2.bin floppy/stage2.bin
